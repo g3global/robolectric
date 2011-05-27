cf8934838356a0fdc57861900e00406375e6fac0
unknown <kpotgieter@.G3GLOBAL.local>
2011-05-27 10:45:45 +0100

    Added a Shadow Log to display log messages to the console.

---
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowLog.java
@@ -0,0 +1,48 @@
+package com.xtremelabs.robolectric.shadows;
+
+import java.text.MessageFormat;
+
+import android.util.Log;
+
+import com.xtremelabs.robolectric.internal.Implements;
+
+
+@Implements(Log.class)
+public class ShadowLog {
+	
+	private static final String messageFormat = "ANDROID LOG:\n\tLEVEL:{0} TAG:{1} Message:{2}";
+	private static final String DEBUG_LEVEL = "DEBUG";
+	private static final String INFO_LEVEL = "INFO";
+	private static final String WARNING_LEVEL = "WARN";
+	private static final String VERBOSE_LEVEL = "VERBOSE";
+	
+	
+	public static int d(String tag, String message){
+		writeMessage(DEBUG_LEVEL, tag, message);
+		return 0;
+	}
+
+	public static int i(String tag, String message){
+		writeMessage(INFO_LEVEL, tag, message);
+		return 0;
+	}
+	
+	public static int w(String tag, String message){
+		writeMessage(WARNING_LEVEL, tag, message);
+		return 0;
+	}
+	
+	public static int v(String tag, String message){
+		writeMessage(VERBOSE_LEVEL, tag, message);
+		return 0;
+	}
+	
+	private static void writeMessage(String level, String tag, String message){
+		System.out.println(buildLogMessageForDisplay(level, tag, message));
+	}
+
+	static String buildLogMessageForDisplay(String level, String tag,
+			String message) {
+		return MessageFormat.format(messageFormat, level, tag, message);
+	}
+}
